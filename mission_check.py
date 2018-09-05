fa6bf5d163b4ae1e47612eb91d4fbe4fc37b2357
Flowers Safety <40meta@gmail.com>
2018-09-05 19:02:37 +0600

    prints mission author

--- mission_check.py
+++ mission_check.py
@@ -28,33 +28,39 @@ with open('/home/yay/a3/wog_96_the_forgotten_war_latest.lingor3/mission.sqm') as
 
             #print class_path
 
-        elif (len(class_path) and class_path[0] == str('Mission')):
-            if (class_name == 'Intel'):
+        elif (len(class_path)):
+            splitted_attribute_definition = line.strip().decode('utf-8').split(' = ')
 
-                splitted_attribute_definition = line.strip().decode('utf-8').split(' = ')
+            if (len(splitted_attribute_definition) == 2):
+                attr_name, attr_value = splitted_attribute_definition
 
-                #print splitted_attribute_definition
-                
-                if (len(splitted_attribute_definition) == 2):
-                    attr_name, attr_value = splitted_attribute_definition
+                # in the mission file strings strings are in quotes, ends with semi
+                stripped_attr_value = attr_value[1:-2]
 
-                    # in the mission file strings strings are in quotes, ends with semi
-                    stripped_attr_value = attr_value[1:-2]
+                if (class_path[0] == str('ScenarioData')):
+                    if (attr_name == 'author'):
+                        print stripped_attr_value
 
-                    if (attr_name == 'briefingName'):
-                        #print attr_value
+                elif (class_path[0] == str('Mission')):
+                    if (class_name == 'Intel'):
+                        #print splitted_attribute_definition
+                        
+                        if (len(splitted_attribute_definition) == 2):
 
-                        # 2
-                        if (not re.match('WOG \d{2,3} (\w+\ )+\d\.\d$', stripped_attr_value, re.UNICODE)):
-                            print 'Название миссии не удовлетворяет шаблону.'.encode('utf-8')
+                            if (attr_name == 'briefingName'):
+                                #print attr_value
 
-                        # 2.1
-                        #if (not stripped_attr_value.startswith('WOG ')):
-                        #    #print ('Название миссии должно начинаться с "WOG".'.encode('utf-8')
-                        #    print ('Название миссии должно начинаться с "WOG". (%s)' % (stripped_attr_value)).encode('utf-8'),
+                                # 2
+                                if (not re.match('WOG \d{2,3} (\w+\ )+\d\.\d$', stripped_attr_value, re.UNICODE)):
+                                    print 'Название миссии не удовлетворяет шаблону.'.encode('utf-8')
 
-                        #if (attr_value.decode('utf-8').split(' ')):
-                            
+                                # 2.1
+                                #if (not stripped_attr_value.startswith('WOG ')):
+                                #    #print ('Название миссии должно начинаться с "WOG".'.encode('utf-8')
+                                #    print ('Название миссии должно начинаться с "WOG". (%s)' % (stripped_attr_value)).encode('utf-8'),
 
-                    #if (attr_name == 'overviewText'):
-                        #print attr_name, attr_value.encode('utf-8')
+                                #if (attr_value.decode('utf-8').split(' ')):
+                                    
+
+                            #if (attr_name == 'overviewText'):
+                                #print attr_name, attr_value.encode('utf-8')
