b7b27a5c5bcde9593fe7cb346d68b9889bc90d41
Flowers Safety <40meta@gmail.com>
2018-09-11 01:55:20 +0600

    pbo filename check was added

--- check.sh
+++ check.sh
@@ -7,30 +7,37 @@
 
 #echo $1
 
-extractpbo_msg_last_line=`extractpbo $1 |& tail -n 1` 
+#filename_check=`sed -n -E '/^wog_[0-9]{2,3}_[a-z0-9_]+_[0-9]{2}\.[A-Za-z0-9_]+\.pbo$/p' <<< $1`
+filename_check=`sed -n -E '/^wog_[0-9]{2,3}_[a-z0-9_]+_[0-9]{2}\.[A-Za-z0-9_]+\.pbo$/p' <<< "wog_156_voshod_10.Takistan.pbo"`
 
-if [[ $extractpbo_msg_last_line != "No Error(s)" ]];
-    then echo $extractpbo_msg_last_line;
-
-    else {
-        # get the folder name from .pbo filename
-        mission_folder=`sed s/\.pbo$// <<< $1`;
-
-        #echo $mission_folder;
-
-        mission_sqm=$mission_folder/mission.sqm
-
-        # check if mission.sqm was binarize
-        # get the second line and search RE in it
-        mission_was_deraped=`head -n 2 $mission_sqm | grep "^//DeRap: wog_"`
-
-        if [[ -z $mission_was_deraped ]];
-            then echo "mission.sqm wasn't binarized!";
-            
-            else python2 mission_check.py;
-        fi
-    }
+if [[ -z $filename_check ]];
+    then echo 'wrong mission filename.';
 fi
+
+#extractpbo_msg_last_line=`extractpbo $1 |& tail -n 1` 
+#
+#if [[ $extractpbo_msg_last_line != "No Error(s)" ]];
+#    then echo $extractpbo_msg_last_line;
+#
+#    else {
+#        # get the folder name from .pbo filename
+#        mission_folder=`sed s/\.pbo$// <<< $1`;
+#
+#        #echo $mission_folder;
+#
+#        mission_sqm=$mission_folder/mission.sqm
+#
+#        # check if mission.sqm was binarize
+#        # get the second line and search RE in it
+#        mission_was_deraped=`head -n 2 $mission_sqm | grep "^//DeRap: wog_"`
+#
+#        if [[ -z $mission_was_deraped ]];
+#            then echo "mission.sqm wasn't binarized.";
+#            
+#            else python2 mission_check.py;
+#        fi
+#    }
+#fi
 
 # TODO: check size of the images
 # filesize in KB
