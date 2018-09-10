01e63208404a2fa766f19ff4c2c26ab830fc8c55
Flowers Safety <40meta@gmail.com>
2018-09-10 23:34:49 +0600

    get the mission folder name from the filename

--- check.sh
+++ check.sh
@@ -9,13 +9,19 @@
 
 extractpbo_msg_last_line=`extractpbo $1 |& tail -n 1` 
 
-if [[ $extractpbo_msg_last_line == "No Error(s)" ]];
-    then echo k;
-    else echo $extractpbo_msg_last_line;
-fi
+if [[ $extractpbo_msg_last_line != "No Error(s)" ]];
+    then echo $extractpbo_msg_last_line;
 
-# check if mission.sqm was binarize
-#head -n 2 ~/a3/wog_96_the_forgotten_war_latest.lingor3/mission.sqm | grep "^//DeRap: wog_"
+    else {
+        # get the folder name from .pbo filename
+        mission_folder=`sed s/\.pbo$// <<< $1`;
+
+        echo $mission_folder;
+
+        # check if mission.sqm was binarize
+        #head -n 2 ~/a3/wog_96_the_forgotten_war_latest.lingor3/mission.sqm | grep "^//DeRap: wog_"
+    }
+fi
 
 
 #python2 mission_check.py
