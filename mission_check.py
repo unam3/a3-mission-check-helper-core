d267c689c699f4be4f81b37884221d9077750797
Flowers Safety <40meta@gmail.com>
2018-10-11 18:03:21 +0600

    Enable target debug and respawn rulesets checks was added

--- mission_check.py
+++ mission_check.py
@@ -41,15 +41,37 @@ with open(path_to_mission_sqm) as opened_mission_file:
 
                 stripped_semi_attr_value = attr_value[:-1]
 
+                #general attr
                 if (class_path[0] == str('ScenarioData')):
                     if (attr_name == 'author'):
-                        print 'Автор миссии:', stripped_attr_value
+                        print 'Author:', stripped_attr_value
+
+                    if (attr_name == 'loadScreen'):
+                        print 'Load screen image:', stripped_attr_value
 
                     if (attr_name == 'saving' and stripped_semi_attr_value != '0'):
                         print 'Сохранение должно быть выключено.'.encode('utf-8')
 
-                    #if (attr_name == 'respawn' and not stripped_semi_attr_value == '0'):
-                    #    print 'Сохранение должно быть выключено.'.encode('utf-8')
+                    if (attr_name == 'respawn' and stripped_semi_attr_value != '1'):
+                        print 'Respawn must be set to "Spectator".'
+
+                elif (class_path[0] == str('CustomAttributes')):
+
+                    #general attr
+                    if ((len(class_path) == 6 and class_path[1] == 'Category0' and class_path[2] == 'Attribute0'
+                        and class_path[3] == 'Value' and class_path[4] == 'data' and class_path[5] == 'value' 
+                        and attr_name == 'items' and stripped_semi_attr_value != str(1))
+                        or (len(class_path) == 8 and class_path[1] == 'Category0' and class_path[2] == 'Attribute0'
+                        and class_path[3] == 'Value' and class_path[4] == 'data' and class_path[5] == 'value'
+                        and class_path[6] == 'Item0' and class_path[7] == 'data'
+                        and attr_name == 'value' and stripped_attr_value != 'Spectator')):
+
+                            print 'Only "Spectator" option must be set in the "Rulesets" of the "Multiplayer" attributes'
+                    #general attr
+                    if (len(class_path) == 3 and class_path[1] == 'Category1' and class_path[2] == 'Attribute0'
+                        and attr_name == 'property' and stripped_attr_value != 'EnableTargetDebug'):
+
+                            print 'Please turn on "Enable Target Debugging"'
 
                 elif (class_path[0] == str('Mission')):
                     if (class_name == 'Intel'):
@@ -58,7 +80,7 @@ with open(path_to_mission_sqm) as opened_mission_file:
                         if (len(splitted_attribute_definition) == 2):
 
                             if (attr_name == 'briefingName'):
-                                #print attr_value
+                                print 'Mission name:', attr_value
 
                                 # 2
                                 if (not re.match('WOG \d{2,3} (\w+\ )+\d\.\d$', stripped_attr_value, re.UNICODE)):
@@ -72,5 +94,6 @@ with open(path_to_mission_sqm) as opened_mission_file:
                                 #if (attr_value.decode('utf-8').split(' ')):
                                     
 
-                            #if (attr_name == 'overviewText'):
-                                #print attr_name, attr_value.encode('utf-8')
+                            # side (color, attack) - side (color, def)
+                            if (attr_name == 'overviewText'):
+                                print stripped_attr_value.encode('utf-8')
