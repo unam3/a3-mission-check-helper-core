d90cb66f531380d8efe3fca6e20894f9a0d8cfcf
Flowers Safety <40meta@gmail.com>
2019-01-02 17:10:18 +0600

    rewriting check.sh in python: is mission.sqm binarized?

---
+++ check_binarization.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+
+def was_mission_binarized(path_to_uploaded_file):
+
+    mission_sqm_path = ''.join([path_to_uploaded_file[:-4], '/mission.sqm'])
+
+    reference_line_part = '//DeRap: wog_'
+
+    with open(mission_sqm_path, 'r') as f:
+    
+        for i, line in enumerate(f):
+            
+            # we interested only in second line with signature
+            if (i == 1):
+                
+                #print line
+
+                #for n, word in enumerate(line):
+                #    
+                #    print n, word, reference_line_part[n], word == reference_line_part[n]
+
+                return line.startswith(reference_line_part)
